f2245c8f5f4187b1ee99cf861e395d0c1ab5e38a
hibaa <fathimaibaa@gmail.com>
2024-11-13 20:34:32 +0530

    fist

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser :true,
+            useUnifiedTopology :true
+         });
         console.log(`mongoDB connected: ${conn.connection.host}`)
     } catch (error) {
-        console.log(`Error: ${error.message}`)
+        console.log(`Error: ${error}`)
         process.exit(1)
     }
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,9 +1,11 @@
-import { Container, Card, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Card, Button, Form } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Hero = () => {
+ 
   return (
-    <div className=' py-5'>
+    <div className='py-5'>
       <Container className='d-flex justify-content-center'>
         <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
           <h1 className='text-center mb-4'>MERN Authentication</h1>
@@ -12,16 +14,18 @@ const Hero = () => {
             an HTTP-Only cookie. It also uses Redux Toolkit and the React
             Bootstrap library
           </p>
-          <div className='d-flex'>
-            <LinkContainer to ='/login'>
-                <Button variant='primary' className='me-3'>
-                     Sign In
-                 </Button>
+           
+
+          <div className='mt-3 d-flex'>
+            <LinkContainer to='/login'>
+              <Button variant='primary' className='me-3'>
+                Sign In
+              </Button>
             </LinkContainer>
-            <LinkContainer to ='/register'>
-                <Button variant='secondary' href='/register'>
-                    Signup
-                </Button>
+            <LinkContainer to='/register'>
+              <Button variant='secondary' href='/register'>
+                Signup
+              </Button>
             </LinkContainer>
           </div>
         </Card>
